5e4cb64ec69318b446dcb2aaa020adfeb35a4eb1
xmarosi <xmarosi@stuba.sk>
2022-10-20 00:25:41 +0200

    more

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 console.log('Script loaded');
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        console.log('Card was clicked');
+        card.classList.toggle('checked');
+        cards.forEach(otherCard => {
+            if (otherCard !== card) {
+                otherCard.classList.remove('checked');
+            }
+        });
+    });
+});
